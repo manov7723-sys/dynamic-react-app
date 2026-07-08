2aea49f621abb36ff2f3c2a245c5a8b3dadd083e
manov7723-sys <manov7723@gmail.com>
2026-07-08 12:45:20 +0530

    Delete iac/aws/eks directory

--- iac/aws/eks/environments/prod/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "agent-demo-bucket7723"
-    key    = "eks/app-cluster-prod/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
--- iac/aws/eks/environments/staging/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "agent-demo-bucket7723"
-    key    = "eks/app-cluster-staging/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
