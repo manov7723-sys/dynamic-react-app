c2a3eb9efb69ee52c56424964a2814fa856a9053
manov7723-sys <manov7723@gmail.com>
2026-07-20 16:57:46 +0530

    Add Jenkins jenkins (us-east-1)

--- terraform/jenkins/jenkins/versions.tf
+++ terraform/jenkins/jenkins/versions.tf
@@ -2,7 +2,6 @@ terraform {
   required_version = ">= 1.4"
   required_providers {
     aws = { source = "hashicorp/aws", version = "~> 5.60" }
-    tls = { source = "hashicorp/tls", version = "~> 4.0" }
   }
 }
 
