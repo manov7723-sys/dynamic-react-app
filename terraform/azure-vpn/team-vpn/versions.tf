a48bd344c1f6f4f6bdb0681276c5e2c336832e49
manov7723-sys <manov7723@gmail.com>
2026-07-22 14:55:24 +0530

    Add Azure OpenVPN team-vpn (centralus)

---
+++ terraform/azure-vpn/team-vpn/versions.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_version = ">= 1.4"
+  required_providers {
+    azurerm = { source = "hashicorp/azurerm", version = "~> 3.100" }
+    tls     = { source = "hashicorp/tls",     version = "~> 4.0" }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
